6a3c335a7efe2a9853be51f12fa2a56ab0e7e0f6
Johann Schopplich <mail@johannschopplich.com>
2022-10-10 18:38:57 +0200

    feat: improve locale messages types

--- src/types.ts
+++ src/types.ts
@@ -1,19 +1,23 @@
-import type { App, Ref } from 'vue'
+import type { App, ComputedRef, Ref } from 'vue'
 
-export type Messages = Record<string, any>
+export type Messages = Record<string, Record<string, any>>
 
 export interface I18nConfig {
   defaultLocale?: string
   locales?: string[]
-  messages: Messages
+  messages?: Messages
 }
 
 export interface I18nInstance {
-  locales?: string[]
+  defaultLocale: string
   locale: Ref<string>
+  locales: string[]
   messages: Messages
-  t: (key: string, params?: any) => string
+  t: (key: string, params?: any) => ComputedRef<string>
   setLocale: (locale: string) => void
   getLocale: () => string
+  addMessages: (newMessages: Messages) => void
   install(app: App): void
 }
+
+export type UseI18n = Omit<I18nInstance, 'install'>
