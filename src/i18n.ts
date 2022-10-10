03cc20e68cb5707a067df851bd8e49cc13b6bf20
Johann Schopplich <mail@johannschopplich.com>
2022-10-10 18:39:28 +0200

    feat!: translation helper `t()` returns computed getter

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,37 +1,40 @@
-import { ref } from 'vue'
+import { computed, reactive, ref } from 'vue'
 import type { App, InjectionKey } from 'vue'
-import type { I18nConfig, I18nInstance, Messages } from './types'
+import { recursiveRetrieve } from './utils'
+import type { I18nConfig, I18nInstance, Messages, UseI18n } from './types'
 
-export const injectionKey = Symbol('i18n') as InjectionKey<I18nInstance>
+export const injectionKey = Symbol('i18n') as InjectionKey<UseI18n>
 
 export function createI18n(config: I18nConfig): I18nInstance {
-  const { defaultLocale, locales, messages } = config
-  const fallbackLocale = 'en'
-  const locale = ref(defaultLocale || fallbackLocale)
+  const { defaultLocale = 'en' } = config
+  const messages = reactive(config.messages || {})
+  const locale = ref(defaultLocale)
+  const locales = config.locales || (Object.keys(messages).length ? Object.keys(messages) : [locale.value])
 
   const t = (key: string, params?: Record<string, any>) => {
-    const pack = messages[locale.value] || messages[fallbackLocale]
+    return computed(() => {
+      if (typeof key !== 'string') {
+        console.warn('[i18n]', `Message "${key}" must be a string`)
+        return ''
+      }
 
-    if (typeof key !== 'string') {
-      console.warn('[i18n]', `Message "${key}" must be a string`)
-      return ''
-    }
-
-    try {
-      return recursiveRetrieve(key.split('.'), pack, params)
-    }
-    catch (error) {
-      console.warn('[i18n]', error)
-      return ''
-    }
+      try {
+        return recursiveRetrieve(key.split('.'), messages[locale.value], params)
+      }
+      catch (error) {
+        console.warn('[i18n]', error)
+        return ''
+      }
+    })
   }
 
   const setLocale = (loc: string) => {
     if (!messages[loc]) {
       console.warn(
         '[i18n]',
-        `Messages for "${loc}" not found, falling back to "${fallbackLocale}"`,
+        `Messages for locale "${loc}" not found. Available locale message: ${Object.keys(messages).join(', ')}`,
       )
+      return
     }
 
     locale.value = loc
@@ -39,87 +42,28 @@ export function createI18n(config: I18nConfig): I18nInstance {
 
   const getLocale = () => locale.value
 
+  const addMessages = (newMessages: Messages) => {
+    for (const loc of Object.keys(newMessages)) {
+      if (!messages[loc])
+        messages[loc] = {}
+
+      Object.assign(messages[loc], newMessages[loc])
+    }
+  }
+
   return {
-    locales,
+    defaultLocale,
     locale,
+    locales,
     messages,
     t,
     setLocale,
     getLocale,
+    addMessages,
     install(app: App) {
       app.provide(injectionKey, this)
       app.config.globalProperties.$t = this.t
       app.config.globalProperties.$i18n = this
     },
   }
-}
-
-function parseAndReplaceString(
-  str: string,
-  params: Record<string, any>,
-): string {
-  const TEMPLATE_RE = /{(\w*)}/g
-  let arr: RegExpExecArray | null
-  let _str = str
-
-  // eslint-disable-next-line no-cond-assign
-  while ((arr = TEMPLATE_RE.exec(str)) !== null) {
-    if (!Object.prototype.hasOwnProperty.call(params, arr[1]))
-      throw new Error(`Param "${arr[1]}" not found`)
-
-    _str = _str.replace(arr[0], params[arr[1]])
-  }
-
-  return _str
-}
-
-function recursiveRetrieve(
-  chain: string[],
-  messages: Messages,
-  params?: Record<string, any>,
-): string {
-  const key = chain[0]
-
-  if (key.includes('[')) {
-    // Get array item
-    const [objKey, rest] = key.split('[')
-    const num = parseInt(rest.replace(']', ''))
-
-    if (num < 0) {
-      throw new Error(
-        `Invalid array index "${num}" for message "${chain.join('.')}"`,
-      )
-    }
-
-    const message = messages[objKey][num]
-
-    if (
-      !messages[objKey]
-      && messages[objKey].length > 0
-      && message
-      && message !== ''
-    )
-      throw new Error(`Message "${chain.join('.')}" not found`)
-
-    if (chain.length === 1)
-      return typeof message === 'string' ? message : ''
-
-    return recursiveRetrieve(chain.slice(1), message, params)
-  }
-
-  const message = messages[key]
-
-  if (!message && message !== '')
-    throw new Error(`Message "${chain.join('.')}" not found`)
-
-  if (chain.length === 1) {
-    let str: string = typeof message === 'string' ? message : ''
-
-    if (params)
-      str = parseAndReplaceString(str, params)
-
-    return str
-  }
-
-  return recursiveRetrieve(chain.slice(1), message, params)
 }
---
+++ src/utils.ts
@@ -0,0 +1,72 @@
+import type { Messages } from './types'
+
+export function recursiveRetrieve(
+  chain: string[],
+  messages: Messages,
+  params?: Record<string, any>,
+): string {
+  const key = chain[0]
+
+  if (key.includes('[')) {
+    // Get array item
+    const [objKey, rest] = key.split('[')
+    const num = parseInt(rest.replace(']', ''))
+
+    if (num < 0) {
+      throw new Error(
+          `Invalid array index "${num}" for message "${chain.join('.')}"`,
+      )
+    }
+
+    const message = messages[objKey][num]
+
+    if (
+      !messages[objKey]
+        && messages[objKey].length > 0
+        && message
+        && message !== ''
+    )
+      throw new Error(`Message "${chain.join('.')}" not found`)
+
+    if (chain.length === 1)
+      return typeof message === 'string' ? message : ''
+
+    return recursiveRetrieve(chain.slice(1), message, params)
+  }
+
+  const message = messages[key]
+
+  if (!message && message !== '')
+    throw new Error(`Message "${chain.join('.')}" not found`)
+
+  if (chain.length === 1) {
+    let str: string = typeof message === 'string' ? message : ''
+
+    if (params)
+      str = parseAndReplaceString(str, params)
+
+    return str
+  }
+
+  return recursiveRetrieve(chain.slice(1), message, params)
+}
+
+function parseAndReplaceString(
+  str: string,
+  params: Record<string, any>,
+): string {
+  const TEMPLATE_RE = /{(\w*)}/g
+  let arr: RegExpExecArray | null
+  let _str = str
+
+  // eslint-disable-next-line no-cond-assign
+  while ((arr = TEMPLATE_RE.exec(str)) !== null) {
+    if (!Object.prototype.hasOwnProperty.call(params, arr[1]))
+      throw new Error(`Param "${arr[1]}" not found`)
+
+    _str = _str.replace(arr[0], params[arr[1]])
+  }
+
+  return _str
+}
+
